cc7a8ba175f330ddddd55e45fda88334b1b69950
TimLee <timleenights@gmail.com>
2017-09-28 22:21:33 +0800

    Start project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/TimLee/Documents/Develop/Projects/Python/simple_blog/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
